50bff1ccf082f652642d6042959421eaa37a5a01
Erico Gomes <jerick.gs@gmail.com>
2020-07-13 01:35:50 -0300

    Api desenvolvida

---
+++ src/config/mail.js
@@ -0,0 +1,17 @@
+/**
+ * Alguns servidores de e-mail
+ * Amazon SES - Mailgun - Sparkpost - Mandril(mailchimp) - Mailtrap(DEV)
+*/
+
+export default {
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
+  secure: false,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
---
+++ src/config/sentry.js
@@ -0,0 +1,4 @@
+export default {
+  // dsn: 'https://5896a24b552641c7ab19a0467ac932dd@o399117.ingest.sentry.io/5255751',
+  dsn: process.env.SENTRY_DSN,
+};
